e9e6da13aa6bd157feb1ad09df4a0126e1d68f1a
Sebastian Buczek <sebastian.buczek92@gmail.com>
2019-08-28 17:00:53 +0200

    Convert to MVC design pattern

---
+++ ToDoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  ToDoey
+//
+//  Created by Sebastian Buczek on 28/08/2019.
+//  Copyright © 2019 Sebastian Buczek. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title:  String = ""
+    var done: Bool = false
+}
